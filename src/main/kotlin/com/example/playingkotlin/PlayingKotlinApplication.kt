e98970be5d2b8c3fe87f34fa018b44faaece2dec
Yuuki Ooguro <kitkat.jaja@gmail.com>
2019-05-30 17:11:06 +0900

    first commit

---
+++ src/main/kotlin/com/example/playingkotlin/PlayingKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.playingkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlayingKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<PlayingKotlinApplication>(*args)
+}
