d0526b5bb3ea5f8df4b45286b6bf6c056c7922fe
Yuuki Ooguro <kitkat.jaja@gmail.com>
2019-05-31 10:26:48 +0900

    クリックするとカウントされるやつ

--- src/main/kotlin/com/example/playingkotlin/controller/HelloController.kt
+++ src/main/kotlin/com/example/playingkotlin/controller/HelloController.kt
@@ -1,13 +1,22 @@
 package com.example.playingkotlin.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class HelloController {
-    
+    var count: Int = 0
+
     @GetMapping("hello")
     fun hello(): String {
+        return "hello"
+    }
+    
+    @GetMapping("count")
+    fun count(model: Model): String {
+        count++
+        model.addAttribute("count", count);
         return "hello"
     }
 }
