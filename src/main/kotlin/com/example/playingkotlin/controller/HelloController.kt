5c632d64cedba17a4f34f0c1eae9736ca0a43bc9
Yuuki Ooguro <kitkat.jaja@gmail.com>
2019-05-31 09:44:51 +0900

    Hello画面表示

---
+++ src/main/kotlin/com/example/playingkotlin/controller/HelloController.kt
@@ -0,0 +1,13 @@
+package com.example.playingkotlin.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HelloController {
+    
+    @GetMapping("hello")
+    fun hello(): String {
+        return "hello"
+    }
+}
